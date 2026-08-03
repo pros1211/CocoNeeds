492b8ccf2016b8ed4c76bf688cd3849f2eebaced
pros1211 <phelixprospero@gmail.com>
2026-08-03 21:08:03 +0700

    .

--- lib/tourGuide/homeDash.ts
+++ lib/tourGuide/homeDash.ts
@@ -1,6 +1,6 @@
-import { DriveStep } from "driver.js";
-export const farmerHomeGuide: DriveStep[] = [
-  {
-    element: [`data-tour=greeting`],
-  },
-];
+// import { DriveStep } from "driver.js";
+// export const farmerHomeGuide: DriveStep[] = [
+//   {
+//     element: [`data-tour=greeting`],
+//   },
+// ];
